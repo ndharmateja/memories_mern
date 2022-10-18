71dedadd09ba2cc75d27ded684cc0c33e89685ba
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-18 13:49:19 +0530

    c: Add app layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { AppBar, Container, Grid, Grow, Typography } from "@material-ui/core";
 import React from "react";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
+import memories from "./images/memories.png";
 
 const App = () => {
   return (
@@ -10,7 +11,23 @@ const App = () => {
         <Typography variant="h2" align="center">
           Memories
         </Typography>
+        <img src={memories} alt="memories" height="60%" />
       </AppBar>
+      <Grow in>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form />
+          </Grid>
+        </Grid>
+      </Grow>
     </Container>
   );
 };
