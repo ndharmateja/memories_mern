cedaae703d817fdca2dc1286eff701b502bd1a6f
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-19 18:18:14 +0530

    c: Remove localhost base url in api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,4 @@
 import axios from "axios";
 
-const url = "http://localhost:3003";
-
-export const fetchPosts = () => axios.get(`${url}/posts`);
-export const createPost = () => axios.post(`${url}/posts`);
+export const fetchPosts = () => axios.get(`/posts`);
+export const createPost = () => axios.post(`/posts`);
