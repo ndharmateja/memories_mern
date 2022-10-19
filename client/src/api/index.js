c45dd76316b656c7b25cbd4f5a0faf0dc80a2600
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-19 19:12:15 +0530

    c: Add api function to create post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
 export const fetchPosts = () => axios.get(`/posts`);
-export const createPost = () => axios.post(`/posts`);
+export const createPost = (newPost) => axios.post(`/posts`, newPost);
