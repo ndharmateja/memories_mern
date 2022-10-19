13c118ea1d492ddcd59c833cd9366fb086cdca43
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-19 18:08:58 +0530

    c: Add methods in api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,4 +2,5 @@ import axios from "axios";
 
 const url = "http://localhost:3003";
 
-export const fetchPosts = () => axios.get(new URL(url, `${url}/posts`));
+export const fetchPosts = () => axios.get(`${url}/posts`);
+export const createPost = () => axios.post(`${url}/posts`);
