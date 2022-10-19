32884d625a0296dc02b61f0616932c4ef3e0ef69
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-19 19:37:37 +0530

    c: Add grid to show the posts

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -3,14 +3,30 @@ import Post from "./Post/Post";
 import useStyles from "./styles";
 import { useSelector } from "react-redux";
 
+import { Grid, CircularProgress } from "@material-ui/core";
+
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
+
   return (
     <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
+      {posts.length === 0 ? (
+        <CircularProgress />
+      ) : (
+        <Grid
+          className={classes.container}
+          container
+          alignItems="stretch"
+          spacing={3}
+        >
+          {posts.map((post) => (
+            <Grid key={post.id} item xs={12} sm={6}>
+              <Post post={post} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </>
   );
 };
