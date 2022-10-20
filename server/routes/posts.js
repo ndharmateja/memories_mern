6b757606837329fd930ce9da74874944a4f5b967
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-20 11:51:00 +0530

    s: Add mergeParams to post router

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { createPost, getPosts, updatePost } from "../controllers/posts.js";
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getPosts).post(createPost);
 router.route("/:id").patch(updatePost);
