ee4b984825b9e58f8ba3a4f91d1d04fae9567a62
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-20 13:59:23 +0530

    c: Fix bug in updatePost api call

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const fetchPosts = () => axios.get(`/posts`);
 export const createPost = (newPost) => axios.post(`/posts`, newPost);
-export const updatePost = (id, updatedPost) =>
-  axios.patch(`/posts/${id}`, updatedPost);
+export const updatePost = (updatedPost) =>
+  axios.patch(`/posts/${updatedPost.id}`, updatedPost);
