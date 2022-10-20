9fb71acc2898ba1c754a2503ca837157827f2351
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-20 14:11:44 +0530

    c: Add deletePost api fn

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,3 +4,4 @@ export const fetchPosts = () => axios.get(`/posts`);
 export const createPost = (newPost) => axios.post(`/posts`, newPost);
 export const updatePost = (updatedPost) =>
   axios.patch(`/posts/${updatedPost.id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`/posts/${id}`);
