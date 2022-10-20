fe8f506a3fec55327b8ee2fda41f8f3267fdde27
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-20 14:11:26 +0530

    s: Integrate controller with the route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createPost, getPosts, updatePost } from "../controllers/posts.js";
+import {
+  createPost,
+  deletePost,
+  getPosts,
+  updatePost,
+} from "../controllers/posts.js";
 
 const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getPosts).post(createPost);
-router.route("/:id").patch(updatePost);
+router.route("/:id").patch(updatePost).delete(deletePost);
 
 export default router;
