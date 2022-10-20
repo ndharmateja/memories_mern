f37940533ebf0a6d42c4865ac283ec870e06c425
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-20 12:01:34 +0530

    c: Add api service to update post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,3 +2,5 @@ import axios from "axios";
 
 export const fetchPosts = () => axios.get(`/posts`);
 export const createPost = (newPost) => axios.post(`/posts`, newPost);
+export const updatePost = (id, updatedPost) =>
+  axios.patch(`/posts/${id}`, updatedPost);
