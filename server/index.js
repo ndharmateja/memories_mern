4e97fdf316eb3c8f6816f99d0693ddcf68e5bc2d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-21 07:09:57 +0530

    s: Change order of imports

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import * as dotenv from "dotenv";
 import postsRouter from "./routes/posts.js";
 import morgan from "morgan";
+
+import * as dotenv from "dotenv";
 dotenv.default.config();
 
 const app = express();
